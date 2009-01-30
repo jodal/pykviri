eb9da1d422b5450eb3554ce52946d3f50b399225
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-01-30 15:47:18 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'PyKviri',
+    version = '0.1',
+    description = 'LINQ for object in Python',
+    url = 'http://code.jodal.no/git/?p=pykviri.git',
+    author = 'Stein Magnus Jodal',
+    author_email = 'stein.magnus@jodal.no',
+    license = 'GPLv2',
+    py_modules = ['pykviri'],
+)
